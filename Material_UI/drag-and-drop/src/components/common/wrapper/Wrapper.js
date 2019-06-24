45b27078aa6828abbb83df139595a72a5fedc813
Mohammad Zain Abbas <zain.abbas@carepvtltd.com>
2019-06-24 14:44:53 +0500

    Added Kawaii planet components

---
+++ Material_UI/drag-and-drop/src/components/common/wrapper/Wrapper.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Wrapper(props) {
+  return React.createElement("div", {
+    ...props,
+    style: { position: "relative", ...(props.style || {}) }
+  });
+}
+
+export default Wrapper;
--- Material_UI/drag-and-drop/src/index.js
+++ Material_UI/drag-and-drop/src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Backpack from "./components/backpack/Backpack";
+import Browser from "./components/browser/Browser";
+import Cat from "./components/cat/Cat";
+import CreditCard from "./components/creditCard/CreditCard";
+import File from "./components/file/File";
+import Ghost from "./components/ghost/Ghost";
+import IceCream from "./components/iceCream/IceCream";
+import Mug from "./components/mug/Mug";
+import Planet from "./components/planet/Planet";
+import SpeechBubble from "./components/speechBubble/SpeechBubble";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export {
+  Backpack,
+  Browser,
+  Cat,
+  CreditCard,
+  File,
+  Ghost,
+  IceCream,
+  Mug,
+  Planet,
+  SpeechBubble
+};
