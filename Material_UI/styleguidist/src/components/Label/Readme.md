84ecdb4c463989430b5b868cfc5c0c232250268f
Mohammad Zain Abbas <zain.abbas@carepvtltd.com>
2019-06-25 12:27:58 +0500

    New components added to styleguidist

---
+++ Material_UI/styleguidist/src/components/Label/Readme.md
@@ -0,0 +1,13 @@
+Basic Label:
+
+```jsx
+<Label>Hi there !!!</Label>
+```
+
+Pink background label:
+
+```jsx
+<Label background="deeppink" color="white">
+  Click Me
+</Label>
+```
--- Material_UI/styleguidist/styleguide.config.js
+++ Material_UI/styleguidist/styleguide.config.js
@@ -1,3 +1,26 @@
+const path = require("path");
+const { version } = require("./package");
+
 module.exports = {
-  components: "src/components/**/*.js"
+  components: "src/components/**/[A-Z]*.js",
+  defaultExample: true,
+  moduleAliases: {
+    "rsg-example": path.resolve(__dirname, "src")
+  },
+  version,
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        },
+        {
+          test: /\.css$/,
+          loader: "style-loader!css-loader"
+        }
+      ]
+    }
+  }
 };
