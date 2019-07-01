ba919c6c4743ffb3ed21520d38a9421c93550c84
Mohammad Zain Abbas <zain.abbas@carepvtltd.com>
2019-07-01 12:10:35 +0500

    Reducer successfully read and updated the store based upon action

--- redux-test/src/index.js
+++ redux-test/src/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducer = (state, action) => {
+  if (action.type === "changeState") {
+    return action.payload.newState;
+  }
+  return "State";
+};
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+const action = {
+  type: "changeState",
+  payload: {
+    newState: "New State"
+  }
+};
+
+store.dispatch(action);
+
+console.log(store.getState());
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
