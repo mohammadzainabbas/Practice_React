2e30d42ebc5e54f2c9dd60ac69f814fe74f597c7
Mohammad Zain Abbas <zain.abbas@carepvtltd.com>
2019-07-15 17:12:52 +0500

    These two lines of code find the <div> we added to our HTML in the first step, and then display our “Like” button React component inside of it.

--- From Official documentation/1_html_with_react/like_button.js
+++ From Official documentation/1_html_with_react/like_button.js
@@ -20,3 +20,8 @@ class LikeButton extends React.Component {
     );
   }
 }
+
+// ... the starter code you pasted ...
+
+const domContainer = document.querySelector("#like_button_container");
+ReactDOM.render(e(LikeButton), domContainer);
