4b4e33207da5955d398558e2715eae095a8dbe6e
Mohammad Zain Abbas <zain.abbas@carepvtltd.com>
2019-07-22 18:09:15 +0500

    Added zoom image crop preview + drag and drop

--- image-croppie/src/component/ImageZoomCrop.js
+++ image-croppie/src/component/ImageZoomCrop.js
@@ -3,21 +3,45 @@ import Dropzone from "react-dropzone";
 import ReactAvatarEditor from "react-avatar-editor";
 
 class ImageZoomCrop extends Component {
-	handleDrop = dropped => {
+	state = {
+		image: null
+	};
+	handleOnDrop = dropped => {
 		this.setState({ image: dropped[0] });
 	};
 	render() {
+		const maxSize = 2 * 1024 * 1024;
+		const { image } = this.state;
 		return (
-			<Dropzone
-				onDrop={this.handleDrop}
-				disableClick
-				style={{ width: "250px", height: "250px" }}>
-				<ReactAvatarEditor
-					width={250}
-					height={250}
-					image={this.state.image}
-				/>
-			</Dropzone>
+			<>
+				<Dropzone
+					maxSize={maxSize}
+					accept="image/jpeg, image/png"
+					onDrop={this.handleOnDrop}
+					multiple={false}
+					onDragEnter={e => console.log("On DragOver")}
+					onDragOver={e => console.log("On DragOver")}
+					onDragLeave={e => console.log("On DragOver")}>
+					{({ getRootProps, getInputProps }) => (
+						<section>
+							<div {...getRootProps()}>
+								<input {...getInputProps()} />
+								<p>
+									Drag 'n' drop some files here, or click to
+									select files
+								</p>
+							</div>
+						</section>
+					)}
+				</Dropzone>
+				{image ? (
+					<ReactAvatarEditor
+						width={250}
+						height={250}
+						image={this.state.image}
+					/>
+				) : null}
+			</>
 		);
 	}
 }
