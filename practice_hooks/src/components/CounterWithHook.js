b1ccb3bddc3466ec5d55d60c5fbfe5d4fd1959e7
Mohammad Zain Abbas <zain.abbas@carepvtltd.com>
2019-11-06 18:43:08 +0500

    Bit better implementation

--- practice_hooks/src/components/CounterWithHook.js
+++ practice_hooks/src/components/CounterWithHook.js
@@ -2,16 +2,17 @@ import React, { useState } from "react";
 
 function Counter() {
 	// Declare a new state variable, which we'll call "count"
-	const [{ counter1, counter2 }, setCounter] = useState({ counter1: 0, counter2: 0 });
+	const [count1, setCount1] = useState(0);
+	const [count2, setCount2] = useState(0);
 
 	return (
 		<div>
-			<p>You clicked first counter {counter1} times</p>
-			<button onClick={() => setCounter({ counter1: counter1 + 1, counter2 })}>Increment in counter 1</button>
-			<button onClick={() => setCounter({ counter1: counter1 - 1, counter2 })}>Decrement in counter 1</button>
-			<p>You clicked second counter {counter2} times</p>
-			<button onClick={() => setCounter({ counter1, counter2: counter2 + 1 })}>Increment in counter 2</button>
-			<button onClick={() => setCounter({ counter1, counter2: counter2 - 1 })}>Decrement in counter 2</button>
+			<p>You clicked first counter {count1} times</p>
+			<button onClick={() => setCount1(count1 + 1)}>Increment in counter 1</button>
+			<button onClick={() => setCount1(count1 - 1)}>Decrement in counter 1</button>
+			<p>You clicked second counter {count2} times</p>
+			<button onClick={() => setCount2(count2 + 1)}>Increment in counter 2</button>
+			<button onClick={() => setCount2(count2 - 1)}>Decrement in counter 2</button>
 		</div>
 	);
 }
