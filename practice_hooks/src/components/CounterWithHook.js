7be31539afd7d5ca87b3b3c09dda7848bab9901c
Mohammad Zain Abbas <zain.abbas@carepvtltd.com>
2019-11-06 18:36:00 +0500

    Extended functionality a bit

--- practice_hooks/src/components/CounterWithHook.js
+++ practice_hooks/src/components/CounterWithHook.js
@@ -2,12 +2,16 @@ import React, { useState } from "react";
 
 function Counter() {
 	// Declare a new state variable, which we'll call "count"
-	const [count, setCount] = useState(0);
+	const [{ counter1, counter2 }, setCounter] = useState({ counter1: 0, counter2: 0 });
 
 	return (
 		<div>
-			<p>You clicked {count} times</p>
-			<button onClick={() => setCount(count + 1)}>Click me</button>
+			<p>You clicked first counter {counter1} times</p>
+			<button onClick={() => setCounter({ counter1: counter1 + 1, counter2 })}>Increment in counter 1</button>
+			<button onClick={() => setCounter({ counter1: counter1 - 1, counter2 })}>Decrement in counter 1</button>
+			<p>You clicked second counter {counter2} times</p>
+			<button onClick={() => setCounter({ counter1, counter2: counter2 + 1 })}>Increment in counter 2</button>
+			<button onClick={() => setCounter({ counter1, counter2: counter2 - 1 })}>Decrement in counter 2</button>
 		</div>
 	);
 }
