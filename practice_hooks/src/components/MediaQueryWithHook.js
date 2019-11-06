d6324950d5a81b96d0d485af4bdde15197ea1a98
Mohammad Zain Abbas <zain.abbas@carepvtltd.com>
2019-11-06 13:02:24 +0500

    Minor update -> Added matches to useEffect

--- practice_hooks/src/components/MediaQueryWithHook.js
+++ practice_hooks/src/components/MediaQueryWithHook.js
@@ -11,7 +11,7 @@ const useMediaQuery = query => {
 		let listener = () => setMatches(media.matches);
 		media.addListener(listener);
 		return () => media.removeListener(listener);
-	}, [query]);
+	}, [matches, query]);
 	return matches;
 };
 
