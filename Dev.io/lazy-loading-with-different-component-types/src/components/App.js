a10f1b51dd4f73ecc6c50b4ade5ad9e59b372bf9
Mohammad Zain Abbas <zain.abbas@carepvtltd.com>
2019-11-07 14:45:20 +0500

    Updated App.js

--- Dev.io/lazy-loading-with-different-component-types/src/components/App.js
+++ Dev.io/lazy-loading-with-different-component-types/src/components/App.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { Component, Suspense } from "react";
+import shortid from "shortid";
+import * as Events from "./events";
+
 import "./App.css";
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+class App extends Component {
+	render() {
+		const { events } = this.props;
+		const components = events.map(event => {
+			const Component = Events[event.type];
+			const key = shortid.generate();
+
+			return Component ? <Component key={key} {...event} /> : <Events.NullEvent key={key} />;
+		});
+
+		return (
+			<Suspense fallback={<div>Loading...</div>}>
+				<h2>Sung's GitHub Events</h2>
+				<div>{components}</div>
+			</Suspense>
+		);
+	}
 }
 
 export default App;
