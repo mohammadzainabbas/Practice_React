e7f59d1b91f2eb18049ba5a7739d030f7c59a9a1
Mohammad Zain Abbas <zain.abbas@carepvtltd.com>
2019-11-07 14:50:10 +0500

    Added ForkEvent

---
+++ Dev.io/lazy-loading-with-different-component-types/src/components/events/ForkEvent.js
@@ -0,0 +1,18 @@
+import React from "react";
+const ForkEvent = ({ created_at: eventDate, repo, org, actor, payload }) => {
+	const { display_login: login, url: actorURL } = actor;
+	const { name: repoName, url: repoURL } = repo;
+
+	return (
+		<div class="event">
+			<h3>
+				Fork - (<small>{eventDate.toString()}</small>)
+			</h3>
+			<p>
+				<a href={actorURL}>{login}</a> has forked <a href={repoURL}>{repoName}</a>
+			</p>
+		</div>
+	);
+};
+
+export default ForkEvent;
