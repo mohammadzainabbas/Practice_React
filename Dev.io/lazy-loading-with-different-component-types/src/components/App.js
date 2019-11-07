408e6dd928102963f7f99b1fe1fb2d06cf61d26e
Mohammad Zain Abbas <zain.abbas@carepvtltd.com>
2019-11-07 16:50:14 +0500

    Added UserInfo

--- Dev.io/lazy-loading-with-different-component-types/src/components/App.js
+++ Dev.io/lazy-loading-with-different-component-types/src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component, Suspense } from "react";
 import shortid from "short-id";
 import * as Events from "./events";
 import "./App.css";
+import UserInfo from "./customHooks/getUserInfo";
 
 class App extends Component {
 	render() {
@@ -12,17 +13,12 @@ class App extends Component {
 
 			return Component ? <Component key={key} {...event} /> : <Events.NullEvent key={key} />;
 		});
+		debugger;
 
 		return (
 			<Suspense fallback={<div>Loading...</div>}>
-				<div className="profileDiv">
-					<img
-						className="image"
-						src={events.map(item => item)[0].actor.avatar_url}
-						alt={events.map(item => item)[0].actor.login}
-					/>
-					<h2 className="text">{events.map(item => item)[0].actor.login}'s GitHub Events</h2>
-				</div>
+				<UserInfo profile_link={events.map(item => item)[0].actor.login} />
+				<h2 className="text">{events.map(item => item)[0].actor.login}'s GitHub Events</h2>
 				<div>{components}</div>
 			</Suspense>
 		);
