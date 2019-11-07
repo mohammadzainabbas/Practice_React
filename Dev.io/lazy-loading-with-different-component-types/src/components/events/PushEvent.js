1127b191bb56b7aaf4cd66334c6158c9e924c6cc
Mohammad Zain Abbas <zain.abbas@carepvtltd.com>
2019-11-07 14:53:55 +0500

    Added PushEvent

---
+++ Dev.io/lazy-loading-with-different-component-types/src/components/events/PushEvent.js
@@ -0,0 +1,34 @@
+import React from "react";
+import shortid from "shortid";
+
+const PushEvent = ({ created_at: eventDate, repo, org, actor, payload }) => {
+	const { display_login: login, url: actorURL } = actor;
+	const { commits } = payload;
+	const { name: repoName, url: repoURL } = repo;
+
+	return (
+		<div class="event">
+			<h3>
+				Push - (<small>{eventDate}</small>)
+			</h3>
+			<div>
+				<a href={actorURL}>{login}</a> has pushed to <a href={repoURL}>{repoName}</a>
+				<Commits commits={commits} />
+			</div>
+		</div>
+	);
+};
+
+const Commits = ({ commits }) => {
+	return (
+		<ol>
+			{commits.map(commit => (
+				<li key={shortid.generate()}>
+					{commit.author.name} has commited with message "{commit.message}"
+				</li>
+			))}
+		</ol>
+	);
+};
+
+export default PushEvent;
