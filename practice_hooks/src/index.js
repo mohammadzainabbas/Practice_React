1438275d688aa412d9d5507cfd73b10bdfd83eb4
Mohammad Zain Abbas <zain.abbas@carepvtltd.com>
2019-11-11 11:45:15 +0500

    Updated index.js

--- practice_hooks/src/index.js
+++ practice_hooks/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/CounterWithHook";
+import App from "./components/SearchResults";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
