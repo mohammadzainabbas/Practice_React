a8290115bf0ba2eb9f80b5dcf00108a613735738
Mohammad Zain Abbas <zain.abbas@carepvtltd.com>
2019-11-11 11:38:58 +0500

    Used Async Await with useEffect() hook

---
+++ practice_hooks/src/components/SearchResults.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
+/**
+ * This example includes data fetching using useEffect() hook
+ */
+function SearchResults() {
+	const [data, setData] = useState({ hits: [] });
+	const [query, setQuery] = useState("react");
+
+	useEffect(() => {
+		let ignore = false;
+
+		async function fetchData() {
+			const result = await axios("https://hn.algolia.com/api/v1/search?query=" + query);
+			if (!ignore) setData(result.data);
+		}
+
+		fetchData();
+		return () => {
+			ignore = true;
+		};
+	}, [query]);
+
+	return (
+		<>
+			<input value={query} onChange={e => setQuery(e.target.value)} />
+			<ul>
+				{data.hits.map(item => (
+					<li key={item.objectID}>
+						<a href={item.url}>{item.title}</a>
+					</li>
+				))}
+			</ul>
+		</>
+	);
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<SearchResults />, rootElement);
