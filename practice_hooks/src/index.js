c5cf2bf569e0c0e697c3be8ce273fefd4849c873
Mohammad Zain Abbas <zain.abbas@carepvtltd.com>
2019-11-12 16:41:14 +0500

    Updated index.js

--- practice_hooks/src/index.js
+++ practice_hooks/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/SearchResults";
+import App from "./components/Try_to_write_custom_useState";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
