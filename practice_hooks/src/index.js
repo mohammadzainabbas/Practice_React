81a1672763e2cc56423f84d71ea802957c4f4399
Mohammad Zain Abbas <zain.abbas@carepvtltd.com>
2019-11-13 11:45:02 +0500

    For initial render

--- practice_hooks/src/index.js
+++ practice_hooks/src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from "./serviceWorker";
 export const renderWithCrappyHooks = () => {
   ReactDOM.render(<App />, document.getElementById("root"));
 };
-
+renderWithCrappyHooks();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
