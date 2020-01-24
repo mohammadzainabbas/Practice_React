009a907860aed6c19b0c25809d5951717b3118eb
Mohammad Zain Abbas <zain.abbas@carepvtltd.com>
2020-01-24 16:11:38 +0500

    Add two numbers

---
+++ WebAssembly/wasm_node_try/Hello_world.js
@@ -0,0 +1,16 @@
+const iw = require("inline-webassembly");
+
+const wasm = `;;wasm
+(module
+    (func (export "add") (param $n1 i32) (param $n2 i32) (result i32)
+      get_local $n1
+      get_local $n2
+      i32.add
+      )
+)
+`;
+
+iw(wasm).then(wasmModule => {
+	const sum = wasmModule.add(44, 99);
+	console.log(`Sum = ${sum}`); // 143
+});
