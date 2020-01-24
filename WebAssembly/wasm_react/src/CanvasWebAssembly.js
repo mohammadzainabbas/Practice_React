8379e6530084802b16a025c99a6dfbd46ca96135
Mohammad Zain Abbas <zain.abbas@carepvtltd.com>
2020-01-24 15:36:29 +0500

    Converted to functional component

--- WebAssembly/wasm_react/src/CanvasWebAssembly.js
+++ WebAssembly/wasm_react/src/CanvasWebAssembly.js
@@ -1,23 +1,21 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 
 const wasm = import("./fractal.wasm");
 
-class CanvasWebAssembly extends Component {
-	constructor(props) {
-		super(props);
-		this.canvasRef = React.createRef();
-	}
-	componentDidMount() {
+const CanvasWebAssembly = props => {
+	const canvasRef = React.createRef();
+
+	useEffect(() => {
 		wasm.then(wasm => {
 			const mandelIterWASM = wasm._Z10mandelIterffi;
-			let canvas = this.canvasRef.current.getContext("2d");
+			let canvas = canvasRef.current.getContext("2d");
 			let mag = 200;
 			let panX = 2;
 			let panY = 1.25;
 			let maxIter = 100;
 
-			for (let x = 10; x < this.props.height; x++) {
-				for (let y = 10; y < this.props.width; y++) {
+			for (let x = 10; x < props.height; x++) {
+				for (let y = 10; y < props.width; y++) {
 					// let m = this.mandelIter(x/mag - panX, y/mag - panY, maxIter);
 					let m = mandelIterWASM(x / mag - panX, y / mag - panY, maxIter);
 					canvas.fillStyle = m === 0 ? "#000" : "hsl(0, 100%, " + m + "%)";
@@ -25,15 +23,13 @@ class CanvasWebAssembly extends Component {
 				}
 			}
 		});
-	}
+	}, []);
 
-	render() {
-		return (
-			<>
-				<canvas ref={this.canvasRef} width={this.props.width} height={this.props.height} />
-			</>
-		);
-	}
-}
+	return (
+		<>
+			<canvas ref={canvasRef} width={props.width} height={props.height} />
+		</>
+	);
+};
 
 export default CanvasWebAssembly;
