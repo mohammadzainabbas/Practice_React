38ae77d37781a41db7166fef3b32025b01a0c38f
Mohammad Zain Abbas <zain.abbas@carepvtltd.com>
2020-01-24 15:09:23 +0500

    Both canvases

--- WebAssembly/wasm_react/src/index.js
+++ WebAssembly/wasm_react/src/index.js
@@ -4,6 +4,12 @@ import ReactDOM from "react-dom";
 import Canvas from "./Canvas";
 import CanvasWebAssembly from "./CanvasWebAssembly";
 
-ReactDOM.render(<Canvas width={500} height={500} />, document.getElementById("app"));
+ReactDOM.render(
+	<>
+		<Canvas width={500} height={500} />
+		<CanvasWebAssembly width={500} height={500} />
+	</>,
+	document.getElementById("app")
+);
 
 module.hot.accept();
