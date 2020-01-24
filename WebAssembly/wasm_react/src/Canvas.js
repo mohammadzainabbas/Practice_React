5d5ec4f7f942899ae1573b61ebaa92c30d610481
Mohammad Zain Abbas <zain.abbas@carepvtltd.com>
2020-01-24 14:49:53 +0500

    Added mandelbrot set cpde

--- WebAssembly/wasm_react/src/Canvas.js
+++ WebAssembly/wasm_react/src/Canvas.js
@@ -10,6 +10,24 @@ class Canvas extends Component {
 		canvas.fillRect(0, 0, 100, 100);
 	}
 
+	mandelIter(x, y, maxIter) {
+		let r = x;
+		let i = y;
+		for (let a = 0; a < maxIter; a++) {
+			let tmpr = r * r - i * i + x;
+			let tmpi = 2 * r * i + y;
+
+			r = tmpr;
+			i = tmpi;
+
+			if (r * i > 5) {
+				return (a / maxIter) * 100;
+			}
+		}
+
+		return 0;
+	}
+
 	render() {
 		return (
 			<>
